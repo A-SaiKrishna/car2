46d02f3353a35ecbcdfae580908f215c78165794
saikrishna <saikrishnaarevarapu@gmail.com>
2023-11-11 10:31:35 +0530

    used map to solve

---
+++ problem4.js
@@ -0,0 +1,6 @@
+export function problem4(inev){
+    const yearArray=inev.map((num)=>{
+        return num["car_year"];
+    });
+    return yearArray;
+}
---
+++ test/testproblem4.js
@@ -0,0 +1,4 @@
+import { problem4 } from "../problem4.js";
+import { inventory } from "../inev.js";
+const year=problem4(inventory);
+console.log(year);
